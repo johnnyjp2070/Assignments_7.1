31eeb7f943d26257bec5bebfc8383ccf521e8086
Johnson <johnnyjp2070@gmail.com>
2018-05-11 12:31:37 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var num1 = -8;
+var num2 = 12.5;
+var num3 = -6.769;
+var num4 = 0;
+var num5 = -1;
+
+if (num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 >= num5){
+    console.log("Greatest Number of the given is "+ num1 );
+} else if (num2 >= num1 && num2 >= num3 && num2 >= num4 && num2 >= num5){
+    console.log("Greatest Number of the given is "+ num2 );
+} else if (num3 >= num1 && num3 >= num2 && num3 >= num4 && num3 >= num5){
+    console.log("Greatest Number of the given is "+ num3 );
+} else if (num4 >= num1 && num4 >= num2 && num4 >= num3 && num4 >= num5){
+    console.log("Greatest Number of the given is "+ num2 );
+} else {
+    console.log("Greatest Number of the given is "+ num5 );
+}
